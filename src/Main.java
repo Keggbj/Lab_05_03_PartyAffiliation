4a467fe7b133c6a6cdce11d8bbb851fa3aeef77a
minik <keggbj@mail.uc.edu>
2022-09-22 16:03:58 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String Platform = "q";
+
+        if (Platform.equals("R")) {
+            System.out.println("You get a Republican Elephant!");
+        } else if (Platform.equals("D")) {
+            System.out.println("You get a Democratic Donkey!");
+        } else if (Platform.equals("I"))
+        {
+
+            System.out.println("You get a Independent Man!");
+        }else
+        {
+        System.out.println("You are a different party!");
+        }
+    }
+}
+
+
